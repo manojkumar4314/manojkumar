96407ab4560f4dc50e1cab51d7ed69dc1ec424e2
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-01-21 16:31:05 +0530

    Create vowcon.c

---
+++ vowcon.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[6]={'a','e','i','o','u'};
+char b;
+printf("enter the alphabet number");
+scanf("%c",&b);
+if(a[0]==b||a[1]==b||a[2]==b||a[3]==b||a[4]=b)
+{
+printf("\n the given alphabet is vowel");
+}
+esle
+{
+printf("\n the given alphbet is constant");
+}
+}
