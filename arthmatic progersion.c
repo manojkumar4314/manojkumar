856e31fa00afb16a8f427a857a0b9585f3b64f62
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-02-06 19:07:10 +0800

    Create arthmatic progersion.c

---
+++ arthmatic progersion.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+   void main()
+   {
+       int a,b,c,d,e,i;
+       printf("enter value a");
+       scanf("%d",&a);
+       printf("\n enter value b");
+       scanf("%d",&b);
+       printf("\n enter value of c");
+       scanf("%d",&c);
+       d=(a*b);
+       e=(d*c);
+       i=(d+e);
+       printf("value of i=%d",i);
+   }
