962c97841dd641a8d90abca2911413f32663b69a
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-02-06 18:34:59 +0800

    Create primnum bt two num.c

---
+++ primnum bt two num.c
@@ -0,0 +1,15 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;num=0;i=0;
+scanf("%d&n);
+for(i=0;i<2;i++)
+{
+if(n%i==0)
+num=num+1
+print (" between intervals is\n");
+scanf("%d"&intervals);
+break;
+}
