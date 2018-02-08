ebfd921368cddf8e9d7ad73a214db21bb3f9f517
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-02-08 16:47:49 +0530

    Create given two times in hour.c

---
+++ given two times in hour.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int b,h,a,r,t,k;
+scanf("%d",&b);
+scanf("%d",&h);
+scanf("%d",&a);
+scanf("%d",&r);
+t=b-h;
+k=a-r;
+printf("%d:%d",t,k);
+return 0;
+}
