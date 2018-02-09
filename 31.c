26f595e17fe4443633fafcbfb4e2dfc03fedab91
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-02-09 18:10:54 +0800

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+int main() 
+{
+	char b[100];
+	int n,i,count=0;
+	scanf(" %[^\t\n]s",b);
+	n=strlen(b);
+	for(i=0;i<n;i++)
+	{
+	 if(b[i]>='b'&&b[i]<='z'||b[i]>='B'&&a[i]<='Z')
+	 {
+	     count++;
+	 }
+	}
+	printf("%d",count);
+	return 0;
+}
