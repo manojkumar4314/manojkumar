d7484da367a3b60b9c9ec1363d69f40c2fbd7304
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-02-22 16:17:50 +0530

    Create 39.C

---
+++ 39.C
@@ -0,0 +1,21 @@
+#include<stdio.h>        
+int main()
+{
+    int n,a[10],i,max;
+    printf("Enter the size of array");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    max=a[0];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}
