b1fd09e72c13e5d3769ec2109fa7652e597d6307
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-03-16 14:09:35 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%7==0)
+    {
+        printf("Yes");
+        
+    }
+    else
+    { 
+        printf("No");
+    }
+    getch();
+}
