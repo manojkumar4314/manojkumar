61516c6c729523a3a684305777b0c8da10d6348f
manojkumar4314 <35652409+manojkumar4314@users.noreply.github.com>
2018-03-20 16:47:56 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+void main() 
+{
+    int n,m,a,i;
+    scanf("%d %d",&n,&m);
+    a=n*m;
+    for(i=0;i<=a;i++)
+    {
+        if(a==(i*i))
+        {
+            printf("yes..perfect square");
+        }
+    }
+getch();
+}
